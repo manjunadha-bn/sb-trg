2d612b8df0af128cf0ddd5c85ee1cadb18c83240
Rajesh Kumar <Rajesh.jangra@gmail.com>
2023-06-23 14:54:07 +0530

    Project structure created

---
+++ src/main/java/com/brane/sbtrg/SbTrgApplication.java
@@ -0,0 +1,13 @@
+package com.brane.sbtrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brane/sbtrg/SbTrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brane.sbtrg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
