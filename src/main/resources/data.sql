789258fa09400f55d78b10cefe2badb4944a1270
Rajesh Kumar <Rajesh.jangra@gmail.com>
2023-07-01 14:37:21 +0530

    Data Insertion on initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Product (id, name, description) VALUES (101, 'Soap', 'Bathing Soap');
+INSERT INTO Product (id, name, description) VALUES (201, 'Pen', 'Roller Ball Pen');
+INSERT INTO Product (id, name, description) VALUES (301, 'Shirt', 'Full Sleeve collared');
+
+INSERT INTO Inventory (id, count, product_id) VALUES (101, 9, 101);
+INSERT INTO Inventory (id, count, product_id) VALUES (201, 7, 201);
+INSERT INTO Inventory (id, count, product_id) VALUES (301, 3, 301);
--- src/test/java/com/brane/sbtrg/SbTrgApplicationTests.java
+++ src/test/java/com/brane/sbtrg/SbTrgApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpMethod;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @DisplayName("Product application tests")
 public class SbTrgApplicationTests {
 
